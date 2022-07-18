d0ae87e40020536335c1fd08f858419bd25a6176
arturzolkowski <artur.zolkowski@wp.pl>
2022-07-18 09:06:52 +0200

    feat: loading data

---
+++ load_data.py
@@ -0,0 +1,63 @@
+import json
+import pickle
+from typing import List
+
+from gensim.models import Phrases
+import pandas as pd
+import spacy
+import swifter
+
+
+def read_txt(path: str) -> str:
+    with open(path) as f:
+        lines = f.readlines()
+    return lines
+
+
+def load_dataframe(path: str) -> pd.DataFrame:
+    df = pd.read_csv(path, index_col=0)
+    assert "text_path" in df.columns
+    df["text"] = df["text_path"].apply(read_txt)
+    return df
+
+
+def process_tokens(
+    doc: pd.Series, nlp: spacy.language.Language, stop_words: List[str]
+) -> List[str]:
+    spacy_text = nlp(doc)
+    return [
+        token
+        for token in spacy_text
+        if not any([token.is_stop, token.is_punct, token.lemma_ in stop_words, not token.is_alpha])
+    ]
+
+
+def process_lemmas(doc: pd.Series) -> List[str]:
+    return [token.lemma_.lower() for token in doc]
+
+
+def _multiply_ngrams(tokens: List[str]):
+    for token in tokens:
+        if " " in token:
+            yield token
+            yield token
+        yield token
+
+
+def process_text(
+    docs: pd.DataFrame,
+    stop_words: List = [],
+    spacy_model: str = "en_core_web_lg",
+) -> pd.DataFrame:
+    nlp = spacy.load(spacy_model)
+    df = docs.copy()
+
+    df["tokens"] = df["text"].swifter.apply(process_tokens, args=(nlp, stop_words))
+    df["lemmas"] = df["tokens"].apply(process_lemmas)
+    ngram_data = df["tokens"].apply(lambda doc: [token.lemma_.lower() for token in doc])
+    bigram = Phrases(ngram_data, min_count=1, delimiter=" ")
+    trigram = Phrases(bigram[ngram_data], min_count=1, delimiter=" ")
+    df["lemmas"] = df["lemmas"].apply(
+        lambda doc: doc + list(_multiply_ngrams(trigram[bigram[doc]]))
+    )
+    return df
