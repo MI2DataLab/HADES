ffdd09000945c2b9c5b282b60a27e0a7f114da54
arturzolkowski <artur.zolkowski@wp.pl>
2022-07-25 18:48:01 +0200

    refactor: add core functions to init

--- mair-policies-comparision/load_data/__init__.py
+++ mair-policies-comparision/load_data/__init__.py
@@ -1,2 +1,1 @@
-from load_data import (load_dataframe, process_lemmas, process_text,
-                       process_tokens, read_txt)
+from .load_data import load_dataframe, process_lemmas, process_text, process_tokens, read_txt
