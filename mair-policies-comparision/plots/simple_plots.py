75f93dd7f6bdbb72b43dd92efd8546c8647c9b81
arturzolkowski <artur.zolkowski@wp.pl>
2022-08-19 00:28:09 +0200

    feat: add save_path to plot functions

--- mair-policies-comparision/plots/simple_plots.py
+++ mair-policies-comparision/plots/simple_plots.py
@@ -1,5 +1,5 @@
 from collections import Counter
-from typing import Dict
+from typing import Dict, Optional
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,13 +7,19 @@ import seaborn as sns
 
 
 def plot_counter(
-    counter: Counter, orient: str = "h", color: str = "lightblue", figsize: tuple = (20, 13)
+    counter: Counter,
+    orient: str = "h",
+    color: str = "lightblue",
+    figsize: tuple = (20, 13),
+    save_path: Optional[str] = None,
 ):
     plt.figure(figsize=figsize)
     keys = [k[0] for k in counter]
     vals = [int(k[1]) for k in counter]
     ax = sns.barplot(x=vals, y=keys, orient=orient, color=color)
     ax.bar_label(ax.containers[0])
+    if save_path is not None:
+        plt.savefig(save_path)
     return ax
 
 
@@ -24,10 +30,13 @@ def plot_counter_lemmas(
     orient: str = "h",
     color: str = "lightblue",
     figsize: tuple = (20, 13),
+    save_path: Optional[str] = None,
 ):
     filtered_lemmas = df.loc[(df[list(filter_dict)] == pd.Series(filter_dict)).all(axis=1)][
         "lemmas"
     ]
     counter = Counter(filtered_lemmas.sum()).most_common(number)
     plot_counter(counter, orient, color, figsize)
+    if save_path is not None:
+        plt.savefig(save_path)
     plt.show()
