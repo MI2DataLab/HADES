fa38b93abe91f42e0300447e3c97d8e311a69842
wi1lku <piotrwil288@gmail.com>
2022-08-18 14:23:05 +0200

    feat: clustering selection

---
+++ app/utils/__init__.py
@@ -0,0 +1,7 @@
+from utils.clustering import calculate_linkage_matrix
+from utils.clustering import get_hierarchical_clusters
+from utils.clustering import get_kmeans_clusters
+from utils.clustering import get_hdbscan_clusters
+from utils.plots import plot_clusters
+from utils.plots import plot_topic_distances
+from utils.plots import plot_topic_distribution
--- app/utils/plots.py
+++ app/utils/plots.py
@@ -1,7 +1,6 @@
+import numpy as np
 import plotly.express as px
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 import plotly.graph_objects as go
 
 
