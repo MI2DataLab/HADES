d27b6004e409543789a28687050c50860d9a7e15
krzyzinskim <mateusz_krzyzinski@wp.pl>
2022-08-28 21:41:07 +0200

    change hierarchical clust criterion to distance

--- app/utils/clustering.py
+++ app/utils/clustering.py
@@ -12,14 +12,23 @@ def calculate_linkage_matrix(
 
 
 def get_hierarchical_clusters(linkage: np.ndarray, t: float = 1.0):
-    return hc.fcluster(linkage, t=t)
-    
+    return hc.fcluster(linkage, t=t, criterion="distance")
+
+
 def get_kmeans_clusters(topic_probs: pd.DataFrame, n_clusters: int, random_state: int = 42):
     kmeans = KMeans(n_clusters=n_clusters, random_state=random_state).fit(topic_probs)
     return kmeans.labels_
 
-def get_hdbscan_clusters(topic_probs: pd.DataFrame, min_cluster_size: int=5, min_samples: int=None,
-                         cluster_selection_epsilon: int=0.0):
-    hdbscan = HDBSCAN(min_cluster_size=min_cluster_size, min_samples=min_samples,
-                     cluster_selection_epsilon=cluster_selection_epsilon).fit(topic_probs)
+
+def get_hdbscan_clusters(
+    topic_probs: pd.DataFrame,
+    min_cluster_size: int = 5,
+    min_samples: int = None,
+    cluster_selection_epsilon: int = 0.0,
+):
+    hdbscan = HDBSCAN(
+        min_cluster_size=min_cluster_size,
+        min_samples=min_samples,
+        cluster_selection_epsilon=cluster_selection_epsilon,
+    ).fit(topic_probs)
     return hdbscan.labels_
