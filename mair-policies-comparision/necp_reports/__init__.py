1d1ac35e202fa9404c4dfda0ca3470fcebd85bea
Stanislaw Gizinski <s.gizinski84@gmail.com>
2022-11-13 14:47:20 +0100

    stop words

---
+++ mair-policies-comparision/necp_reports/__init__.py
@@ -0,0 +1,19 @@
+COUNTRIES_STOPWORDS = ['Austria', 'Austrian', 'Belgium', 'Belgian', 'Bulgaria', 'Bulgarian', 'Czech', 'Cyprus', 'Cypriot', 'Germany', 'German',
+                      'Denmark', 'Danish', 'Estonia', 'Estonian', 'Croatia', 'Croatian', 'Finland', 'Finnish', 'France', 'French', 'Malta', 'Maltese',
+                      'Luxembourg', 'Lithuania', 'Lithuanian', 'Latvia', 'Latvian', 'Italy', 'Italian', 'Ireland', 'Irish', 'Hungary', 'Hungarian',
+                      'Greece', 'Greek', 'Spain', 'Spanish', 'Netherlands', 'Dutch', 'Poland', 'Polish', 'Portugal', 'Portuguese', 'Romania', 'Romanian',
+                      'Sweden', 'Swedish', 'Slovenia', 'Slovenian', 'Slovakia', 'Slovak']
+
+NECP_STOPWORDS =  ['energy', 'figure', 'table', 'plan', "necp", 'national', 'use', "measure", "sector", "climate",
+                     "plan", "dimension", "integrated", "section", "republic", "measures", "policies", "target", "objective", "policy",
+                     "projection", "assessment", "federal", "government"]
+
+
+STOPWORDS = COUNTRIES_STOPWORDS+NECP_STOPWORDS
+PARAGRAPHS = ["Overview and Process for Establishing the Plan",
+              "Impact Assessment of Planned Policies and Measures",
+              "Decarbonisation",
+              "Energy efficiency",
+              "Energy security",
+              "Internal market",
+              "R&I and Competitiveness"]
