1cebb591998d8b65fcc3ee86f44d8da45d4331ff
climate-policy-radar <artur.zolkowski@wp.pl>
2022-11-13 16:43:41 +0100

    update load data funcs

--- mair-policies-comparision/load_data/__init__.py
+++ mair-policies-comparision/load_data/__init__.py
@@ -1,1 +1,8 @@
-from .load_data import load_dataframe, process_lemmas, process_text, process_tokens, read_txt
+from .load_data import (
+    load_dataframe,
+    load_processed_data,
+    process_lemmas,
+    process_text,
+    process_tokens,
+    read_txt,
+)
--- mair-policies-comparision/load_data/load_data.py
+++ mair-policies-comparision/load_data/load_data.py
@@ -1,3 +1,4 @@
+import os
 from typing import List
 
 import pandas as pd
@@ -22,6 +23,24 @@ def load_dataframe(path: str, text_path_col: str = "text_path") -> pd.DataFrame:
     return df
 
 
+def load_processed_data(
+    data_path: str,
+    text_path_col: str = "text_path",
+    stop_words: List = [],
+    spacy_model: str = "en_core_web_lg",
+    processed_filename: str = "data_processed.csv",
+    data_filename: str = "data.csv",
+) -> pd.DataFrame:
+    processed_path = data_path + processed_filename
+    data_path = data_path + data_filename
+    if os.path.isfile(processed_path):
+        processed_data = pd.read_csv(processed_path, index_col=0)
+    else:
+        processed_data = process_text(load_dataframe(data_path, text_path_col), stop_words, spacy_model)
+        processed_data.to_csv(processed_path)
+    return processed_data
+
+
 def process_text(
     docs: pd.DataFrame,
     stop_words: List = [],
@@ -39,6 +58,7 @@ def process_text(
 
     df["lemmas"] = df["lemmas"].apply(lambda doc: doc + list(_multiply_ngrams(trigram[bigram[doc]])))
     return df
+
 
 def convert_country(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
