b97f8ee725ae7534fd348b33f14a819cde346356
emilia_wisnios <wisniosemilia@gmail.com>
2023-02-16 00:44:13 +0100

    refactor + data cleaning

--- mair-policies-comparision/load_data/__init__.py
+++
@@ -1,8 +0,0 @@
-from .load_data import (
-    load_dataframe,
-    load_processed_data,
-    process_lemmas,
-    process_text,
-    process_tokens,
-    read_txt,
-)
--- mair-policies-comparision/plots/__init__.py
+++
@@ -1,1 +0,0 @@
-from .simple_plots import plot_counter, plot_counter_lemmas
